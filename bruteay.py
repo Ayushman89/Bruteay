ee06cf1e57af4fd1d8ab6d01a1f88fc960a6a07f
Ayushman Dutta <139049050+Ayushman89@users.noreply.github.com>
2023-12-27 20:25:14 +0530

    Add files via upload

---
+++ bruteay.py
@@ -0,0 +1,17 @@
+import string 
+
+import random
+
+print("made by Ayushman Dutta")
+
+data_list = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+chardData = list(data_list)
+
+password = str(input("password"))
+
+myguess =""
+while(myguess!=password):
+ myguess = random.choices(chardData, k=len(password))
+ print("".join(myguess))
+ myguess = "" .join(myguess)
+ print("")
